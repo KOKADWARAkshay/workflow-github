ff5d60280cf79bc5588d8c48b63e53fc2b0dbae2
aksha <akshaykokadwar123@gmail.com>
2023-03-29 04:52:23 +0530

    Initial commit

---
+++ src/test/java/com/akshaytask/workflowgithub/WorkflowGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshaytask.workflowgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
